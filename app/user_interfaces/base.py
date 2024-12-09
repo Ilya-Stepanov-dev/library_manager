50fba6130f35800f44c1141ee7dcbff1c8cb4a43
Ilya Stepanov <ilya.stepanov.dev@gmail.com>
2024-12-09 09:51:12 +0300

    Abstract method run added

--- app/user_interfaces/base.py
+++ app/user_interfaces/base.py
@@ -6,11 +6,19 @@ class Interface(ABC):
     @abstractmethod
     def on_enter(self) -> None:
         """The display is called when entering the interface."""
+
         pass
 
     @abstractmethod
     def handle_input(self, input) -> None:
         """Handles the user's input."""
+
+        pass
+    
+    @abstractmethod
+    def run(self) -> None:
+        """Runs the interface"""
+
         pass
 
 def run_interface(interface: Interface) -> None:
